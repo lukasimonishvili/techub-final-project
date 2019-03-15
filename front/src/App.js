2b6eb78ac9af0b8c38e657081fd2fa92a9f3fd92
mishkagitaristi <mishikomamniashvili@gmail.com>
2019-03-15 18:02:28 +0400

    added header & sidebar design & responsive might need some work

--- front/src/App.js
+++ front/src/App.js
@@ -13,9 +13,9 @@ class App extends Component {
     return (
       <>
         <Header />
+        <Sidebar />
         <Login />
         <Register />
-        <Sidebar />
         <CartContainer />
       </>
     );
