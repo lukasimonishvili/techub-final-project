d5be51c0721aa61f1b6d909277ae31e0547e0ffe
lukasimonishvili <37000830+lukasimonishvili@users.noreply.github.com>
2019-03-16 00:28:53 +0400

    herocu test 4

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendfile(path.join((__dirname = "client/build/index.html")));
   });
