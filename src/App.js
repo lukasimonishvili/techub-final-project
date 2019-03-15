c248e8487c5ac57bc80919a663496ca984bc1e84
lukasimonishvili <37000830+lukasimonishvili@users.noreply.github.com>
2019-03-15 22:04:19 +0400

    herocu test push

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Header } from "./components/header/header";
-import {Login} from "./components/login/login";
-import {CartContainer} from "./components/cart/cartContainer";
+import { Login } from "./components/login/login";
+import { CartContainer } from "./components/cart/cartContainer";
 
+//  here is test component for herocu hosting
+class Test extends React.Component {}
+
+// that`s it :)
 
 import { Register } from "./components/register/register";
 import { Sidebar } from "./components/sidebar/sidebar";
