2cff4f06b070150d444bcfc4a64d6e82c5d0da0b
lukasimonishvili <37000830+lukasimonishvili@users.noreply.github.com>
2019-03-16 00:39:12 +0400

    heroku test 6

--- client/src/components/header/headerSearchButton.js
+++ client/src/components/header/headerSearchButton.js
@@ -5,7 +5,7 @@ export const HeaderSearchButton = () => {
     <button
       className="header__search__button"
       onClick={() => {
-        fetch("http://localhost:3001/comment/5c8b832a8b2e5520a3f022c1")
+        fetch("/comment/5c8b832a8b2e5520a3f022c1")
           .then(res => res.json())
           .then(data => {
             console.log(data);
