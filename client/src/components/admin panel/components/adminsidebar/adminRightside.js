fd9da345182adc221be3c10f313ebb65ceb4ad6f
mishkagitaristi <mishikomamniashvili@gmail.com>
2019-03-18 23:24:54 +0400

    s

--- client/src/components/admin panel/components/adminsidebar/adminRightside.js
+++ client/src/components/admin panel/components/adminsidebar/adminRightside.js
@@ -6,6 +6,7 @@ import {Router} from "@reach/router"
 export const AdminRightside = () => {
   return (
     <Router className="admin__rightside">
+    
       <Additem path="/additem"/>
     </Router>
   );
