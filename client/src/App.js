2d1ff3c4d071298677e7d77e1dbe8a60164027bc
Nini Tsalkalamanidze <ninitsalkalamanidze@Ninis-MacBook-Pro.local>
2019-03-18 21:43:44 +0400

    edit/remove user

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import { Register } from "./components/register/register";
 import { Product } from "./components/product/product";
 import { Feedback } from "./components/feedback/feedback";
 import { Comment } from "./components/comment/comment";
-import { Chat } from "./components/contactUs/chat";
+import { Chat } from "./components/contactus/chat";
 import { Footer } from "./components/footer/footer";
 import { SearchDropdownItem } from "./components/searchDropdownItem/searchDropdownItem";
 import { Admin } from "./components/admin panel/components/adminsidebar/admin";
@@ -29,7 +29,7 @@ class App extends Component {
         {/* <CartContainer /> */}
         {/* <SearchDropdownItem /> */}
         {/* <Footer /> */}
-        <Admin/>
+        <Admin />
         {/* <Additem/> */}
       </>
     );
--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const cors = require("cors");
 const path = require("path");
 
 const { connect } = require("./connect");
-const { userRegister, logIn } = require("./controllers/user.controller");
+const {
+  userRegister,
+  logIn,
+  removeUser,
+  editUser
+} = require("./controllers/user.controller");
 const {
   addComment,
   editComment,
@@ -38,6 +43,8 @@ app.use(cors());
 
 app.post("/register", userRegister);
 app.post("/login", logIn);
+app.post("/removeUser", removeUser);
+app.post("/editUser/:userId", editUser);
 
 app.post("/addProduct", uploadProductImg.array("img", 5), addProduct);
 app.post(
