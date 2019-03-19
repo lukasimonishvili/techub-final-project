0587b853544e90bf9e209f38fa7aded28722ee7a
mishkagitaristi <mishikomamniashvili@gmail.com>
2019-03-19 23:28:04 +0400

    style fixes adminpanel customer list

--- client/src/components/admin panel/components/user list/adminCustomerlist.js
+++ client/src/components/admin panel/components/user list/adminCustomerlist.js
@@ -2,12 +2,14 @@ import React from "react"
 import { AdminCustomerlistTitle } from "./adminCustomerlistTitle";
 import { AdminCustomerlistSearch } from "./adminCustomerlistSearch";
 import { AdminCustomerlistTable } from "./adminCustomerlistTable";
+import { AdminCustomerlistSearchBtn } from "./adminCustomerlistSearchBtn";
 
 export const AdminCustomerlist = () => { 
     return     (
     <div className="customerlist">
         <AdminCustomerlistTitle />
         <AdminCustomerlistSearch />
+        <AdminCustomerlistSearchBtn/>
         <AdminCustomerlistTable />
     </div>)
 
---
+++ client/src/components/admin panel/components/user list/adminCustomerlistSearchBtn.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const AdminCustomerlistSearchBtn = () => {
+    return <button className="customerlist__search--button">Search</button>
+}
