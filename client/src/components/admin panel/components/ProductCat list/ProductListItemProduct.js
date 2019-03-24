22d3573d1c09941f17ace303e6b4a26395efd0ab
mishkagitaristi <mishikomamniashvili@gmail.com>
2019-03-24 15:41:41 +0400

    product list in admin side finished might need some work

--- client/src/components/admin panel/components/ProductCat list/ProductListItemProduct.js
+++ client/src/components/admin panel/components/ProductCat list/ProductListItemProduct.js
@@ -1,5 +1,63 @@
 import React from "react";
 
 export const ProductListItemProduct = () => {
-  return <div className="" />;
+  return (
+    <div className="productlist__edit">
+      <div className="productlist__edit__images fl fl_jus_bet">
+        <div className="productlist__edit__images--item">
+          <img src="https://www.w3schools.com/w3css/img_lights.jpg" />
+        </div>
+        <div className="productlist__edit__images--item">
+          <img src="https://www.w3schools.com/w3css/img_lights.jpg" />
+        </div>
+        <div className="productlist__edit__images--item">
+          <img src="https://www.w3schools.com/w3css/img_lights.jpg" />
+        </div>
+        <div className="productlist__edit__images--item">
+          <img src="https://www.w3schools.com/w3css/img_lights.jpg" />
+        </div>
+        <div className="productlist__edit__images--item">
+          <img src="https://www.w3schools.com/w3css/img_lights.jpg" />
+        </div>
+      </div>
+      <div className="additem">
+        <input
+          className="additem__upload"
+          type="file"
+          multiple
+          accept="image/png, image/jpeg"
+          name="upliad images"
+          placeholder="upliad images"
+        />
+        <input
+          className="additem__name"
+          type="text"
+          name="product name"
+          placeholder="product name"
+        />
+        <select className="additem__category" name="product category">
+          <option value="product category">product category</option>
+          <option value="product category 1">product category 1</option>
+        </select>
+        <textarea
+          className="additem__description"
+          name="product description"
+          cols="30"
+          rows="3"
+          placeholder="product description"
+        />
+        <input
+          className="additem__quantity"
+          type="number"
+          placeholder="product quantity"
+        />
+        <input
+          className="additem__cost"
+          type="number"
+          placeholder="product cost"
+        />
+        <button className="additem__send">Save</button>
+      </div>
+    </div>
+  );
 };
--- client/src/components/admin panel/components/ProductCat list/ProductListItems.js
+++ client/src/components/admin panel/components/ProductCat list/ProductListItems.js
@@ -1,29 +1,18 @@
 import React from "react";
+import { ProductListItemProduct } from "./ProductListItemProduct";
 
 export const ProductListItems = () => {
   return (
     <ul className="productlist__list" >
       <li className="productlist__list__item fl fl_jus_bet">
-        some product
-        <div>
+       <div> some product</div>
+        <div className="btn-container">
           <button className="ProductCat__Cat__actionbutton">Edit</button>
           <button className="ProductCat__Cat__actionbutton">Delete</button>
         </div>
       </li>
-      <li className="productlist__list__item fl fl_jus_bet">
-        some product
-        <div>
-          <button className="ProductCat__Cat__actionbutton">Edit</button>
-          <button className="ProductCat__Cat__actionbutton">Delete</button>
-        </div>
-      </li>
-      <li className="productlist__list__item fl fl_jus_bet">
-        some product
-        <div>
-          <button className="ProductCat__Cat__actionbutton">Edit</button>
-          <button className="ProductCat__Cat__actionbutton">Delete</button>
-        </div>
-      </li>
+        <ProductListItemProduct/>
+      
     </ul>
   );
 };
