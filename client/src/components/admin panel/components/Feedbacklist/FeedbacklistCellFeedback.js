d7a4bb0f6daae7a0dd1bdc945ba396a88e9e2dc1
zviadi <z.qafianidze@gmail.com>
2019-03-24 22:35:03 +0400

    Feedback list in admin panel is finished

---
+++ client/src/components/admin panel/components/Feedbacklist/FeedbacklistCellFeedback.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const FeedbacklistCellFeedback = (props) => { 
+    return     (
+    <div className="Feedbacklist__table__Feedback" >
+       {props.TdFeedback}
+    </div>
+    )
+}
---
+++ client/src/components/admin panel/components/Feedbacklist/FeedbacklistCellId.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const FeedbacklistCellId = (props) => { 
+    return     (
+    <div className="Feedbacklist__table__ID" >
+       {props.TdId}
+    </div>
+    )
+}
