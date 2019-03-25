589c86f15e09aedbee7a11e165d7853773ffa4fa
mishkagitaristi <mishikomamniashvili@gmail.com>
2019-03-25 16:30:28 +0400

    comments section styled

--- client/src/components/comment/commentOutput.js
+++ client/src/components/comment/commentOutput.js
@@ -2,11 +2,24 @@ import React from "react";
 
 export const CommentOutput = () => {
   return (
-    <>
-      <span className="comment__output">
-        adgilis sadac gamochndeba komentari
+    <div className="comment__output">
+      <span className="comment__output__comment fl fl_dir_col">
+        adgilis sadac gamochndeba komentari adgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentariadgilis sadac gamochndeba
+        komentariadgilis sadac gamochndeba komentari
+        <span className="comment__output__username">comment written by</span>
       </span>
-      
-    </>
+    </div>
   );
 };
