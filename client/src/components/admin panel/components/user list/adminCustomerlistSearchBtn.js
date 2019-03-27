f1cc27f9fb1bff902598de2d402d6cdbdc0e5cba
mishkagitaristi <mishikomamniashvili@gmail.com>
2019-03-28 01:39:19 +0400

    customer list  remake

--- client/src/components/admin panel/components/user list/adminCustomerlistSearchBtn.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const AdminCustomerlistSearchBtn = () => {
-  return <button className="customerlist__search--button">Search</button>;
-};
--- client/src/components/admin panel/components/user list/adminCustomerlistTableHeader.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const AdminCustomerlistTableHeader = props => {
-  return <th className="customerlist__table__header">{props.collumnName}</th>;
-};
