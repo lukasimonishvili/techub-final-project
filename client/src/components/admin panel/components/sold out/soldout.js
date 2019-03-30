4598c8daa435f2f0aefd1ce5540e53488e3c7943
mishkagitaristi <mishikomamniashvili@gmail.com>
2019-03-30 18:04:17 +0400

    soldout again

--- client/src/components/admin panel/components/sold out/soldout.js
+++ client/src/components/admin panel/components/sold out/soldout.js
@@ -3,6 +3,18 @@ import React from "react";
 export const Soldout = () => {
   return (
     <div className="soldout">
+  <div className="soldout__container fl fl_jus_bet">
+        <img
+          className="soldout__image"
+          src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/gold/iphone8-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795416637"
+        />
+        <span className="soldout__name">item name</span>
+        <input className="soldout__number" type="number" />
+        <div className="soldout__buttons">
+          <button className="soldout__save">Save</button>
+          <button className="soldout__delete">Delete</button>
+        </div>
+      </div>
       <div className="soldout__container fl fl_jus_bet">
         <img
           className="soldout__image"
@@ -10,8 +22,109 @@ export const Soldout = () => {
         />
         <span className="soldout__name">item name</span>
         <input className="soldout__number" type="number" />
-        <button className="soldout__save">Save</button>
-        <button className="soldout__delete">Delete</button>
+        <div className="soldout__buttons">
+          <button className="soldout__save">Save</button>
+          <button className="soldout__delete">Delete</button>
+        </div>
+      </div><div className="soldout__container fl fl_jus_bet">
+        <img
+          className="soldout__image"
+          src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/gold/iphone8-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795416637"
+        />
+        <span className="soldout__name">item name</span>
+        <input className="soldout__number" type="number" />
+        <div className="soldout__buttons">
+          <button className="soldout__save">Save</button>
+          <button className="soldout__delete">Delete</button>
+        </div>
+      </div><div className="soldout__container fl fl_jus_bet">
+        <img
+          className="soldout__image"
+          src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/gold/iphone8-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795416637"
+        />
+        <span className="soldout__name">item name</span>
+        <input className="soldout__number" type="number" />
+        <div className="soldout__buttons">
+          <button className="soldout__save">Save</button>
+          <button className="soldout__delete">Delete</button>
+        </div>
+      </div><div className="soldout__container fl fl_jus_bet">
+        <img
+          className="soldout__image"
+          src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/gold/iphone8-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795416637"
+        />
+        <span className="soldout__name">item name</span>
+        <input className="soldout__number" type="number" />
+        <div className="soldout__buttons">
+          <button className="soldout__save">Save</button>
+          <button className="soldout__delete">Delete</button>
+        </div>
+      </div><div className="soldout__container fl fl_jus_bet">
+        <img
+          className="soldout__image"
+          src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/gold/iphone8-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795416637"
+        />
+        <span className="soldout__name">item name</span>
+        <input className="soldout__number" type="number" />
+        <div className="soldout__buttons">
+          <button className="soldout__save">Save</button>
+          <button className="soldout__delete">Delete</button>
+        </div>
+      </div><div className="soldout__container fl fl_jus_bet">
+        <img
+          className="soldout__image"
+          src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/gold/iphone8-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795416637"
+        />
+        <span className="soldout__name">item name</span>
+        <input className="soldout__number" type="number" />
+        <div className="soldout__buttons">
+          <button className="soldout__save">Save</button>
+          <button className="soldout__delete">Delete</button>
+        </div>
+      </div><div className="soldout__container fl fl_jus_bet">
+        <img
+          className="soldout__image"
+          src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/gold/iphone8-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795416637"
+        />
+        <span className="soldout__name">item name</span>
+        <input className="soldout__number" type="number" />
+        <div className="soldout__buttons">
+          <button className="soldout__save">Save</button>
+          <button className="soldout__delete">Delete</button>
+        </div>
+      </div><div className="soldout__container fl fl_jus_bet">
+        <img
+          className="soldout__image"
+          src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/gold/iphone8-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795416637"
+        />
+        <span className="soldout__name">item name</span>
+        <input className="soldout__number" type="number" />
+        <div className="soldout__buttons">
+          <button className="soldout__save">Save</button>
+          <button className="soldout__delete">Delete</button>
+        </div>
+      </div><div className="soldout__container fl fl_jus_bet">
+        <img
+          className="soldout__image"
+          src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/gold/iphone8-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795416637"
+        />
+        <span className="soldout__name">item name</span>
+        <input className="soldout__number" type="number" />
+        <div className="soldout__buttons">
+          <button className="soldout__save">Save</button>
+          <button className="soldout__delete">Delete</button>
+        </div>
+      </div><div className="soldout__container fl fl_jus_bet">
+        <img
+          className="soldout__image"
+          src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/gold/iphone8-gold-select-2018?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1550795416637"
+        />
+        <span className="soldout__name">item name</span>
+        <input className="soldout__number" type="number" />
+        <div className="soldout__buttons">
+          <button className="soldout__save">Save</button>
+          <button className="soldout__delete">Delete</button>
+        </div>
       </div>
     </div>
   );
