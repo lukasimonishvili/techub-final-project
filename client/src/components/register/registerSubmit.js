3a2fed23a6be1271cd146b3fc265b5b7ac844ba7
Nini Tsalkalamanidze <luka.simonishvili08b@geolab.edu.ge>
2019-03-31 22:10:22 +0400

    now user cant buy from empty cart :D

--- client/src/components/register/registerSubmit.js
+++ client/src/components/register/registerSubmit.js
@@ -1,13 +1,11 @@
 import React from "react";
-// import Axios from "axios";
-// import swal from "sweetalert";
 
 export const RegisterSubmit = props => {
   return (
     <button
       className="register__submit"
-      onClick={()=>{
-        props.register()
+      onClick={() => {
+        props.register();
       }}
     >
       Register
