a3bb3412e6a9657323343496d515e38f16e98bab
mishkagitaristi <mishikomamniashvili@gmail.com>
2019-04-01 17:08:41 +0400

    image zoooooooooooom++++++++++++

--- client/src/components/product/productImageImg.js
+++ client/src/components/product/productImageImg.js
@@ -1,6 +1,19 @@
 import React from "react";
+import ReactImageMagnify from 'react-image-magnify';
 
 export const ProductImageImg = props => {
   let photo = require(`../../img/uploads/${props.mainImg}`);
-  return <img alt="" src={photo} />;
+  return <ReactImageMagnify className="product__image__zoom" {...{
+    smallImage: {
+        alt: 'Wristwatch by Ted Baker London',
+        isFluidWidth: true,
+        src: photo
+    },
+    largeImage: {
+        src: photo,
+        width: 800,
+        height: 800,
+      },
+      enlargedImagePosition: "over" 
+}} />
 };
