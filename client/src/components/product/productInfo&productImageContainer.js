2a83a81f215f3968c8bad1a559e006613df3ce3d
mishkagitaristi <mishikomamniashvili@gmail.com>
2019-04-03 01:30:57 +0400

    display bug last one

--- client/src/components/product/productInfo&productImageContainer.js
+++ client/src/components/product/productInfo&productImageContainer.js
@@ -4,7 +4,7 @@ import { ProductInfo } from "./productInfo";
 
 export const ProductInfoImageContainer = props => {
   return (
-    <div className="product__column1 fl">
+    <div className="product__column1 fl fl_jus_bet">
       <ProductImage
         imgStateHander={props.imgStateHander}
         mainImg={props.mainImg}
